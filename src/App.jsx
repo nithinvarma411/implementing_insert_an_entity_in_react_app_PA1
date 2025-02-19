508b156f566ea16cc22fbb0fce5f9e28db3f84d7
nithinvarma <nithinvarma411@gmail.com>
2025-02-19 11:17:57 +0530

    completed assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 // src/App.jsx
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./Home";
+import AddBook from "./components/AddBook";
+import "./components/AddBook.css";
 
-import React from 'react';
-import Home from './Home';
-import './App.css'; // Import global styles
-
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/add-book" element={<AddBook />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,14 +1,15 @@
 // src/components/Home.jsx
-
 import React from "react";
+import { Link } from "react-router-dom";
 import BookCard from "./components/BookCard";
 import books from "./booksData";
-import "./Home.css"; // Import the CSS for styling
+import "./Home.css";
 
 const Home = () => {
   return (
     <div className="home-container">
       <h1>Book Library</h1>
+      <Link to="/add-book" className="add-book-button">Add Book</Link>
       <div className="books-grid">
         {books.map((book) => (
           <BookCard key={book.id} book={book} />
